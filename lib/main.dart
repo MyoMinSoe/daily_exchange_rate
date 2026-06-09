4ee6931a276705592d0b55ab6394162e2337d8fc
MyoMinSoe <myominsoe.min@gmail.com>
2026-06-09 15:58:11 +0630

    Modify HomeScreen and some code

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,6 @@ void main() => runApp(const ProviderScope(child: MyApp()));
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  // @override
-  // Widget build(BuildContext context, WidgetRef ref) {
-  //   final themeMode = ref.watch(themeProvider);
-  //   return ScreenUtilInit(
-  //     designSize: const Size(360, 690),
-  //     minTextAdapt: true,
-  //     splitScreenMode: true,
-  //     builder: (context, child) => MaterialApp.router(
-  //       routerConfig: routes,
-  //       title: 'Daily Exchange Rate',
-  //       themeMode: themeMode,
-  //       theme: AppThemes.lightTheme,
-  //       darkTheme: AppThemes.darkTheme,
-  //       debugShowCheckedModeBanner: false,
-  //     ),
-  //   );
-  // }
-
-  //without go_router
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final themeMode = ref.watch(themeProvider);
@@ -38,14 +19,33 @@ class MyApp extends ConsumerWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, child) => MaterialApp(
+      builder: (context, child) => MaterialApp.router(
+        routerConfig: routes,
         title: 'Daily Exchange Rate',
         themeMode: themeMode,
         theme: AppThemes.lightTheme,
         darkTheme: AppThemes.darkTheme,
         debugShowCheckedModeBanner: false,
-        home: const HomeScreen(),
       ),
     );
   }
+
+  //without go_router
+  // @override
+  // Widget build(BuildContext context, WidgetRef ref) {
+  //   final themeMode = ref.watch(themeProvider);
+  //   return ScreenUtilInit(
+  //     designSize: const Size(360, 690),
+  //     minTextAdapt: true,
+  //     splitScreenMode: true,
+  //     builder: (context, child) => MaterialApp(
+  //       title: 'Daily Exchange Rate',
+  //       themeMode: themeMode,
+  //       theme: AppThemes.lightTheme,
+  //       darkTheme: AppThemes.darkTheme,
+  //       debugShowCheckedModeBanner: false,
+  //       home: const HomeScreen(),
+  //     ),
+  //   );
+  // }
 }
